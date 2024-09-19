b3481f3e2a754456378792d951f521177e37ba13
Dishant Loriya <sociallensmrb@gmail.com>
2024-09-19 21:54:16 +0530

    fix path

--- src/main/java/com/coupon/CouponController.java
+++ src/main/java/com/coupon/CouponController.java
@@ -3,13 +3,15 @@ package com.coupon;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CouponController {
 
-    @GetMapping("/{code}")
-    public String indexPage(@PathVariable String code) {
-        System.out.println("code: " + code);
+    @RequestMapping(value = "/{code}", method = RequestMethod.GET)
+    public String indexPage(@PathVariable("code") String code) {
+        System.out.println("id: " + code);
         return "index";
     }
 }
