bc04e0e62c92d13e60fc6f6b90f3acc02ba7e881
Dishant Loriya <sociallensmrb@gmail.com>
2024-09-19 21:01:43 +0530

    intial commit

--- src/main/java/com/coupon/CouponController.java
+++ src/main/java/com/coupon/CouponController.java
@@ -2,12 +2,14 @@ package com.coupon;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CouponController {
 
-    @GetMapping("")
-    public String indexPage() {
+    @GetMapping("/{code}")
+    public String indexPage(@PathVariable String code) {
+        System.out.println("code: " + code);
         return "index";
     }
 }
