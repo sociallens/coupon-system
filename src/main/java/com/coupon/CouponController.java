70dd629ad62e8e99139465c09ec32924c4ff4d9c
sociallens <sociallensmrb@gmail.com>
2025-03-22 10:38:38 +0530

    Test Update CouponController.java

--- src/main/java/com/coupon/CouponController.java
+++ src/main/java/com/coupon/CouponController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CouponController {
 
-    @RequestMapping(value = "/{code}", method = RequestMethod.GET)
-    public String indexPage(@PathVariable("code") String code) {
-        System.out.println("id: " + code);
-        return "index";
-    }
+    // @RequestMapping(value = "/{code}", method = RequestMethod.GET)
+    // public String indexPage(@PathVariable("code") String code) {
+    //     System.out.println("id: " + code);
+    //     return "index";
+    // }
 }
